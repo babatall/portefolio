a9235a867002b1e13d99293cfd089ad695577b1f
mor tall <tallpape32@gmail.com>
2024-03-09 00:55:57 +0000

    ajout header

--- index.php
+++ index.php
@@ -4,10 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MorTall</title>
 </head>
 
 <body>
+    <header>
+        <div class="logo">Mor Tall</div>
+        <nav>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">A propos</a></li>
+                <li><a href="">Mes Projet</a></li>
+                <li><a href="">CV</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
 
 </body>
 
